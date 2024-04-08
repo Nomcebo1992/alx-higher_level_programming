5208695829e0bd39e3abd614bfa605937b761f79
Nomcebo1992 <snembambo@gmail.com>
2024-04-08 10:48:59 -0700

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
+const process = require('process');
+
+const args = process.argv;
+
+if (args[2]) {
+  console.log(args[2]);
+} else {
+  console.log('No argument');
+}
